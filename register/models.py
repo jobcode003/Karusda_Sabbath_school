663398873c6084a1695d402a48b72f3c42c901e3
Job Nyaribari <jobnyaribari808@gmail.com>
2026-03-10 11:34:48 +0300

    updated model.py

--- register/models.py
+++ register/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Member(models.Model):
     YEAR_CHOICES = [
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, 'Visitor'),
-        (6, 'Alumni'),
-        (7, 'Staff'),
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', 'Visitor'),
+        ('6', 'Alumni'),
+        ('7', 'Staff'),
     ]
     CLASS_CHOICES = [
         ('Vine branches', 'Vine branches'),
@@ -19,7 +19,7 @@ class Member(models.Model):
     first_name = models.CharField(max_length=100)
     second_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15)
-    year_of_study = models.CharField(choices=YEAR_CHOICES,default=1)
+    year_of_study = models.CharField(choices=YEAR_CHOICES,default='1')
     class_name = models.CharField(max_length=50, choices=CLASS_CHOICES, default='Bible Scholars')
     registration_count = models.PositiveIntegerField(default=1)
 
